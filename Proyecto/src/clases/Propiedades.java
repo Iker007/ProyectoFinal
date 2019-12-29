809d84b272fbd0afb7276ab0ab146bf2f555895b
34619 <34619@LAPTOP-AIMTFCP5>
2019-12-29 19:23:00 +0100

    Propiedades + config inicial

---
+++ Proyecto/src/clases/Propiedades.java
@@ -0,0 +1,39 @@
+package clases;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Propiedades {
+
+	public static void main(String[] args) {
+
+		Properties p = new Properties();
+		p.setProperty("carpeta", "c:\\users\\all\\datos");
+		p.setProperty("usuario", "red");
+		p.setProperty("ventana", "800, 600");
+
+		try {
+			FileOutputStream fos = new FileOutputStream(new File("config.ini"));
+			try {
+				p.store(fos, "Configuración inicial");
+			} catch (IOException e) {
+				System.out.println("No se ha podido escribir en el fichero" + e.getMessage());
+
+			} finally {
+				try {
+					fos.close();
+				} catch (IOException e2) {
+
+				}
+			}
+		} catch (FileNotFoundException e3) {
+			System.out.println("No se ha podido abrir el fichero" + e3.getMessage());
+		}
+
+		System.out.println("Configuración inicial finalizada");
+	}
+
+}
